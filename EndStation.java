f9bc2e9622852d815370a53a323cbd72ebdbd4c5
katyabalin <katya.balin@gmail.com>
2025-03-23 17:02:12 -0400

    added comments to  EndStation.java

--- EndStation.java
+++ EndStation.java
@@ -1,14 +1,18 @@
+// represents a metro station at the end of a line 
 public class EndStation extends Station {
 
+    // sets the name and line for the end station 
     public EndStation(String line, String name) {
         super(line, name);
     }
 
+    //returns string summary of the station
     public String toString()
     {
         return "ENDSTATION " + toStringCommon();
     }
 
+    //updates the connections so that both prev and next refer to the same station
     public void makeEnd() {
         if((next == null) & (prev == null))
             return;
